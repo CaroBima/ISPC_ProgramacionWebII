428dbb53f8f4a00f37e5edf4077217482a857ea5
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-04-27 22:33:58 -0300

    agregando comentarios y cambiando nombres de variables para que sean mas descriptivos

--- 01 - Ej01ProgWeb_Tarjetas/src/main/java/com/mycompany/ej01programacionweb/Ej01ProgramacionWeb.java
+++ 01 - Ej01ProgWeb_Tarjetas/src/main/java/com/mycompany/ej01programacionweb/Ej01ProgramacionWeb.java
@@ -32,44 +32,52 @@ import java.util.Scanner;
 public class Ej01ProgramacionWeb {
 
     public static void main(String[] args) {
-        int opcion = 0;
-        double calculo = 0;
+        int opcionTarjeta = 0; //se inicializa a cero para que ingrese al while
+        double descuento = 0;
+        
+        //se  solicita el ingreso del importe a abonar
         System.out.print("Importe a abonar: ");
         Scanner impor = new Scanner(System.in);
         double importe = impor.nextDouble();
        
-        while( opcion<1 || opcion >3){
+        //se hace el while para comprobar que se ingrese un valor entre 1 y 3
+        //si el valor es menor a 1 o mayor a 3 se sigue repitiendo el ciclo 
+        while( opcionTarjeta<1 || opcionTarjeta >3){
+            //se solicita el ingreso del valor y se modifica el valor de opcion
             System.out.print("Ingrese tipo de tarjeta: ");
             Scanner ingresoTipo = new Scanner(System.in);
-            opcion = ingresoTipo.nextInt();
-            
+            opcionTarjeta = ingresoTipo.nextInt();
         }
         
+        //Ingreso de la cantidad de cuotas
         System.out.print("Cantidad de cuotas: ");
         Scanner inCantCuotas = new Scanner(System.in);
         int cantCuotas = inCantCuotas.nextInt();
         
-        
-        switch(opcion){
+        //calcula el descuento dependiendo del tipo de tarjeta
+        switch(opcionTarjeta){
             case 1:
-                calculo = importe * 0.05;
+                descuento = importe * 0.05;
                 break;
             case 2: 
-                calculo = importe * 0.10;
+                descuento = importe * 0.10;
                 break;
             case 3: 
-                calculo = importe * 0.15;
+                descuento = importe * 0.15;
                 break;
             default:
                  System.out.println("Error en el tipo de tarjeta.");
                  break;      
         }
         
-        double total = importe - calculo;
+        //calcula el importe total a abonar con el descuento aplicado
+        double total = importe - descuento;
+        //calcula el importe de cada cuota
         double importeXCuota = total/cantCuotas;
         
+        //muestra los importes, limitando los decimales a dos
         System.out.println("Importe a abonar con descuento: " + String.format("%.2f", total));
-        System.out.println("Descuento: " +  String.format("%.2f", calculo));
+        System.out.println("Descuento: " +  String.format("%.2f", descuento));
         System.out.println("Cantidad de cuotas: " + cantCuotas);
         System.out.println("Importe por cuota: " + String.format("%.2f", importeXCuota));
     }
