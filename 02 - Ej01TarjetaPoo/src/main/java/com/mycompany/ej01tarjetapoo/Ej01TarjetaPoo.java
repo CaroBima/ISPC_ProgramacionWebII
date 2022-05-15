bf742e81f9213b6d091c3fb66ede5abb1b8ae738
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-05-15 20:09:32 -0300

    corrigiendo error en el calculo del importe total a pagar y de la cuota, terminando el main

--- 02 - Ej01TarjetaPoo/src/main/java/com/mycompany/ej01tarjetapoo/Ej01TarjetaPoo.java
+++ 02 - Ej01TarjetaPoo/src/main/java/com/mycompany/ej01tarjetapoo/Ej01TarjetaPoo.java
@@ -12,6 +12,8 @@ Crear Código para front (javascript) y back (java) modo consola.
 
 package com.mycompany.ej01tarjetapoo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Caro
@@ -19,6 +21,52 @@ package com.mycompany.ej01tarjetapoo;
 public class Ej01TarjetaPoo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+           //llamo al metodo que permite ingresar el tipo de tarjeta:
+          int opcion = 0;
+          double importe = 0;
+          TarjetaCredito tc = new TarjetaCredito();
+          
+          //solicito el ingreso del importe total a abonar:
+          System.out.println("*********************************************");
+          System.out.print  ("       Ingrese el importe: ");
+          Scanner ingImporte = new Scanner(System.in);
+          importe = ingImporte.nextDouble();
+          
+          //se solicita el ingreso del valor y se modifica el valor de opcion
+          System.out.println("*********************************************");
+          System.out.println("*    Cálculo de importe total y cuotas      *");
+          System.out.println("*********************************************");
+          System.out.println("*                                           *");
+          System.out.println("*      Seleccione tarjeta de crédito:       *");
+          System.out.println("*       1 - Platino (5% de recargo)         *");
+          System.out.println("*       2 - Dorada (10% de recargo)         *");
+          System.out.println("*       3 - Black (15% de recargo)          *");
+          System.out.println("*                                           *");
+          System.out.println("*********************************************");
+          System.out.print  ("   Ingrese número de opción (del 1 al 3): ");
+          Scanner ingresoTipo = new Scanner(System.in);
+          int opcionTarjeta = ingresoTipo.nextInt();
+          tc.setTipoTarjeta(opcionTarjeta); //guardo el tipo de tarjeta en la instancia
+          
+          
+          if(tc.getTipoTarjeta() != 0){ //valido que se haya ingresado un numero de tarjeta correcto, si es cero quiere decir que se ingreso un valor distinto a 1, 2 o 3
+          //solicito el ingreso de la cantidad de cuotas:
+          System.out.println("*********************************************");
+          System.out.print  ("       Ingrese la cantidad de cuotas: ");
+          Scanner ingCantCuotas = new Scanner(System.in);
+          int cantCuotas = ingCantCuotas.nextInt();
+                
+          //calculo los importes para mostrarlos
+          double importeTotal = tc.calcularMontoTotal(importe, tc.getTipoTarjeta());//paso el importe ingresado y el tipo de tarjeta ingresado
+          double importeCuota = tc.calcularMontoCuota(importeTotal, cantCuotas);
+
+          System.out.println("*********************************************");
+          System.out.println("   Importe total: $" + importeTotal );
+          System.out.println("   Importe de cada cuota: $" + importeCuota);
+          System.out.println("*********************************************");
+          }else{
+            System.out.println("La opción elegida es incorrecta, debe seleccionarse un número entre 1 y 3");
+            }
+        }
+        
 }
--- 02 - Ej01TarjetaPoo/src/main/java/com/mycompany/ej01tarjetapoo/TarjetaCredito.java
+++ 02 - Ej01TarjetaPoo/src/main/java/com/mycompany/ej01tarjetapoo/TarjetaCredito.java
@@ -5,26 +5,48 @@ package com.mycompany.ej01tarjetapoo;
  * @author Caro
  */
 public class TarjetaCredito {
-    private double interes = 0;
+    private int tipoTarjeta; //se setea en 1, 2 o 3
+    
+ 
+    //devuelve el monto total con intereses sumados
+    public double calcularMontoTotal(double monto, int tipoTarjeta){
+        double montoTotal = 0;
+        double interesT = this.interesDeTarjeta(); //devuelve cual es el interes a pagar, 5%, 10% o 15%
+        montoTotal = monto + (monto * interesT);
+        return montoTotal;
+    }
+    
+    //devuelve el importe de cada cuota
+    public double calcularMontoCuota(double montoTotal, int cantCuotas){
+        return montoTotal/cantCuotas;
+    }
     
     //devuelve el interes correspondiente al tipo de tarjeta
-    public double interesDeTarjeta( int tipoTarjeta){
-        if(tipoTarjeta == 1){
+    public double interesDeTarjeta(){
+        double interes = 0;
+        if(this.tipoTarjeta == 1){
             interes = 0.05;
-        }else if (tipoTarjeta == 2){
+        }else if (this.tipoTarjeta == 2){
             interes = 0.10;
-        }else if (tipoTarjeta == 3){
+        }else if (this.tipoTarjeta == 3){
             interes = 0.15;
+        }else{
+            interes = 0; //devuelve 0 en caso de que se haya ingresado incorrectamente el tipo de tarjeta
         }
         return interes;
     }
     
-    //devuelve el monto total con intereses sumados
-    public double calcularMontoTotal(double monto, int tipoTarjeta, int cantCuotas){
-        double montoTotal = 0;
-        double interesT = this.interesDeTarjeta(tipoTarjeta);
-        montoTotal = monto * interesT;
-        return montoTotal;
+  
+    //constructor, getter y setters
+    public TarjetaCredito() {
+    }
+
+    public int getTipoTarjeta() {
+        return tipoTarjeta;
+    }
+
+    public void setTipoTarjeta(int tipoTarjeta) {
+        this.tipoTarjeta = tipoTarjeta;
     }
     
 }
